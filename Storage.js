bbc3f9f65852457530dda702c1b874ab2bb9fbb8
astrid thijs <r0576736@student.thomasmore.be>
2017-01-04 20:16:56 +0100

    Storage, uitleg

--- Storage.js
+++ Storage.js
@@ -1,77 +1,77 @@
 
 
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;                               //Mongo installeren om te kunnen connecteren
+var url = 'mongodb://localhost:27017/Drones';                                   //hier bevindt onze data ('./Drones)
 
-var url = 'mongodb://localhost:27017/Drones';
 
 var dal = {
 
-	connect: function (error, result) {
+	connect: function (error, result) {                                     //connecteren met mongo
 		MongoClient.connect(url, function (error, db) {
-			if (error)
+			if (error)                                              //als het mis gaat, error
 				throw new Error(error);
-			console.log("Connected successfully to server");
+			console.log("Connected successfully to server");        //anders "Succesfully connected"
 			result(db);
                     });
 		
 	},
         
-        clearDrone: function (call) {
+        clearDrone: function (call) {                                           //'Drone' leegmaken
 		this.connect(null, function (db) {
-			db.collection('drones').drop(function (error, result) {
+			db.collection('drones').drop(function (error, result) { 
 				//callback(result);
-				db.close();
+				db.close();                                     //database 'Drones' sluiten
 			});
 		});
 	},
 	
-        insertDrone: function (drone, callback) {
+        insertDrone: function (drone, callback) {                               //drone toevoegen 
 		this.connect(null, function (db) {
-			db.collection('drones').insert(drone, function (error, result) {
+			db.collection('drones').insert(drone, function (error, result) {    
 				//callback(result);
-				db.close();
+				db.close();                                     //database sleiten
 			});
 		});
 	},
         
-        clearFile: function (call) {
+        clearFile: function (call) {                                            //'File' leegmaken
 		this.connect(null, function (db) {
 			db.collection('files').drop(function (error, result) {
 				//callback(result);
-				db.close();
+				db.close();                                     //database sluiten
 			});
 		});
 	},
         
-        insertFile: function (file, callback) {
+        insertFile: function (file, callback) {                                 //file toevoegen
 		this.connect(null, function (db) {
 			db.collection('files').insert(file, function (error, result) {
 				//callback(result);
-				db.close();
+				db.close();                                     //database sluiten
 			});
 		});
 	},
         
-        clearContent: function (call) {
+        clearContent: function (call) {                                         //'Content' leegmaken
 		this.connect(null, function (db) {
 			db.collection('contents').drop(function (error, result) {
-				//callback(result);
-				db.close();
+				//callback(result);                 
+				db.close();                                     //database sluiten
 			});
 		});
 	},
         
-        insertContent: function (content, callback) {
+        insertContent: function (content, callback) {                           //content toevoegen
 		this.connect(null, function (db) {
 			db.collection('contents').insert(content, function (error, result) {
 				//callback(result);
-				db.close();
+				db.close();                                     //database sluiten
                         });
                 });
             }
         };
 
         
-    module.exports = dal;
+    module.exports = dal;                                                       //alles linken aan variabele 'dal'
     
-    console.log("Check Storage");
+    console.log("Check Storage");                                               //Storage checken
