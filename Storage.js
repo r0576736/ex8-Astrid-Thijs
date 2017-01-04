67fc04ec65cc638872d7dd064f66958aaf00e018
astrid thijs <r0576736@student.thomasmore.be>
2017-01-04 19:47:47 +0100

    Storage Drones gemaakt

---
+++ Storage.js
@@ -0,0 +1,37 @@
+
+
+var MongoClient = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/Drones';
+
+var dal = {
+
+	connect: function (err, result) {
+		MongoClient.connect(url, function (error, db) {
+			if (error)
+				throw new Error(error);
+			console.log("Connected successfully to server");
+			result(db);
+		});
+	},
+        clearDrone: function (call) {
+		this.connect(null, function (db) {
+			db.collection('drones').drop(function (err, result) {
+				//callback(result);
+				db.close();
+			});
+		})
+	},
+	insertDrone: function (drone, callback) {
+		this.connect(null, function (db) {
+			db.collection('drones').insert(drone, function (err, result) {
+				//callback(result);
+				db.close();
+			});
+		});
+	}
+    };
+        
+    module.exports = dal;
+    
+    console.log("Check Storage");
